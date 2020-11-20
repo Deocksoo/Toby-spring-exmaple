fc1f131bbe64ed8145643adc5dc5e7e0165c7863
jaeju.jang <jjj0611@gmail.com>
2020-11-20 21:37:35 +0900

    Add Ch3 example 3-50

--- ch3-final/src/main/java/user/dao/UserDao.java
+++ ch3-final/src/main/java/user/dao/UserDao.java
@@ -64,17 +64,6 @@ public class UserDao {
     }
 
     public int getCount() {
-        return this.jdbcTemplate.query(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-                return con.prepareStatement("Select count(*) from users");
-            }
-        }, new ResultSetExtractor<Integer>() {
-            @Override
-            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-                rs.next();
-                return rs.getInt(1);
-            }
-        });
+        return this.jdbcTemplate.queryForInt("select count(*) from users");
     }
 }
